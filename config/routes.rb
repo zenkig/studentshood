3e1ec1ea85432f3e9158446744e8fd56ab89b024
shilei <heshilei369@gmail.com>
2014-03-06 14:10:16 +0100

    Add a home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Studentshood::Application.routes.draw do
+  get "home/index"
+  get "home/help"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
